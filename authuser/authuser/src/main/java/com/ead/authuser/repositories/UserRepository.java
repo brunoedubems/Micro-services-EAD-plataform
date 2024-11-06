b4cfd56fc29475b6dbe6d665de6b96ef758e29b8
brunoedubems <bemds1@aluno.ifnmg.edu.br>
2024-11-06 18:38:00 -0300

    Filtros avançados

--- authuser/authuser/src/main/java/com/ead/authuser/repositories/UserRepository.java
+++ authuser/authuser/src/main/java/com/ead/authuser/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.ead.authuser.repositories;
 
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 import com.ead.authuser.models.UserModel;
 
-public interface UserRepository extends JpaRepository<UserModel, UUID> {
+public interface UserRepository extends JpaRepository<UserModel, UUID>,JpaSpecificationExecutor<UserModel> {
 
     void save(String usermodel);
     boolean existsByUsername(String username);
